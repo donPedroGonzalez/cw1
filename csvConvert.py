7c89f5517375bbbe53a7629902776c84fc15fa7c
donPedroGonzalez <piotrek_d2@interia.pl>
2024-12-07 15:53:19 +0100

    Correcting the score calculation function

--- csvConvert.py
+++ csvConvert.py
@@ -1,14 +1,32 @@
 import pandas as pd
 
 # Read entire CSV file into a DataFrame
-df = pd.read_csv('D:\\02_NAUKA\\STRONA-CWICZENIA-FRANCUSKIE\\VERIFIED_2022-10-10_2\\missing-element\\JakubGabryel_cw_1.csv',
+df = pd.read_csv("D:\\02_NAUKA\\STRONA-CWICZENIA-FRANCUSKIE\\VERIFIED_2022-10-10_2\\missing-element-cw2\\cw2.csv",
                  delimiter=';',
-                 encoding='utf-8')
+                 encoding='utf-8',
+                 header=0)
 
-
+'''
 print(df.columns)
 print(df.index)
-print(df.values)
+print(df.values[0])
+'''
+
+firstPart = []
+secondPart = []
+missingElement = []
+hints = []
+
+for array in df.values:
+    firstPart.append(array[0])
+    secondPart.append(array[1])
+    missingElement.append(array[2])
+    hints.append(array[3])
+
+print("first part:", firstPart)
+print("second part:", secondPart)
+print("missing element:", missingElement)
+print("hints:", hints)
 
 # Access specific columns
 #print(df['column_name'])
